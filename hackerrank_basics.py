ccd56a7c3d7a4bf0abe6518b6a8ad25e0e5ef537
Sidharth Mishra <sidmishraw@gmail.com>
2016-12-16 19:00:24 -0800

    solved gridland metro problem on hackerrank/

--- hackerrank_basics.py
+++ hackerrank_basics.py
@@ -14,7 +14,7 @@ def draw_staircase():
 
 def circular_array_rotationv1():
   'does the circular array rotation as mentioned in hackerrank problem and solves\
-  the queries. v1.0'
+  the queries. v1.0, this version is easy but slow. Optimized using a pointer'
   n, k, q = list(map(int, input().split(" ")))
   array = list(map(int, input().split(" ")))
   m_s = []
@@ -26,7 +26,7 @@ def circular_array_rotationv1():
   for m in m_s:
     print(array[m])
 
-
+# uses an index pointer pointing to the start after rotation
 def circular_array_rotation():
   'does the circular array rotation as mentioned in hackerrank problem and solves\
   the queries.'
@@ -41,11 +41,45 @@ def circular_array_rotation():
   for m in m_s:
     print(array[(m + start_index) % n])
 
+# train track and lighting problem
+# Gridland metro
+def gridland_metro():
+  'solution for the gridland metro problem on hackerrank'
+  n, m, k = tuple(map(int, input().split(" ")))
+  # grid = [[ 0 for _ in range(0, m, 1)] for _ in range(0, n, 1)]
+  grid = {}
+  post_count = m * n
+  for _ in range(0, k, 1):
+    r, c1, c2 = tuple(map(int, input().split(" ")))
+    if r in grid:
+      c1_old, c2_old = grid[r]
+      if c1 < c1_old and c2 > c2_old:
+        grid[r] = (c1, c2)
+        post_count = post_count - ((c2 - c2_old) + (c1_old - c1))
+      elif c1 < c1_old and c2 <= c2_old:
+        grid[r] = (c1, c2_old)
+        if c1 == c2:
+          post_count = post_count - 1
+        else:
+          post_count = post_count - (c1_old - c1)
+      elif c1 >= c1_old and c2 > c2_old:
+        grid[r] = (c1_old, c2)
+        if c1 == c2:
+          post_count = post_count - 1
+        else:
+          post_count = post_count - (c2 - c2_old)
+    else:
+      grid[r] = (c1, c2)
+      post_count = post_count - (c2 - c1 + 1)
+    print(grid)
+    print(post_count)
 
 
 def main():
   #draw_staircase()
-  circular_array_rotation()
+  #circular_array_rotation()
+  gridland_metro()
 
 if __name__ == '__main__':
   main()
+
