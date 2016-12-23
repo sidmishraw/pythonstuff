d5d9e4cadba3c01b2387dff3485d26d74797adc8
Sidharth Mishra <sidmishraw@gmail.com>
2016-12-22 20:41:44 -0800

    solved tailor shop problem

--- hackerrank_week_27.py
+++ hackerrank_week_27.py
@@ -16,11 +16,32 @@ def drawing_book():
     print((n - p) // 2)
 
 
+# tailor shop problem
+def update_cache(cache, m, mincost):
+  if m not in cache:
+    cache.add(m)
+    return
+  else:
+    return update_cache(cache, m + 1, mincost)
+
+def tailor_shop():
+  'solution for the tailor shop problem on code week 27'
+  import math
+  n, p = tuple(map(int, input().strip().split(" ")))
+  a = list(map(int, input().strip().split(' ')))
+  cache = set()
+  for mincost in a:
+    m = math.ceil(mincost / p)
+    print('m = %s cache = %s' % (m, cache))
+    update_cache(cache, m, mincost)
+  min_nbr = sum(cache)
+  print(min_nbr)
 
 
 def main():
   'main function to execute all the solutions'
-  drawing_book()
+  # drawing_book()
+  tailor_shop()
 
 
 if __name__ == '__main__':
