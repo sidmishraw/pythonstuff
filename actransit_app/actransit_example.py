90c8fdc7ea3475d532ebad78b57560da44a8eb38
Sidharth Mishra <sidmishraw@gmail.com>
2017-01-11 17:34:58 -0800

    added stops apis for AC Transit

--- actransit_app/actransit_example.py
+++ actransit_app/actransit_example.py
@@ -4,7 +4,7 @@
 # @Author: Sidharth Mishra
 # @Date:   2017-01-11 14:48:11
 # @Last Modified by:   Sidharth Mishra
-# @Last Modified time: 2017-01-11 17:08:29
+# @Last Modified time: 2017-01-11 17:32:49
 
 
 __author__ = 'sidmishraw'
@@ -28,10 +28,18 @@ import urllib.response
 import webbrowser
 
 
-
+# STOPS
+# JSON object example - 
+# {
+#     "StopId": 58123,
+#     "Name": "3rd St:Santa Clara Av",
+#     "Latitude": 37.7732681,
+#     "Longitude": -122.2882275,
+#     "ScheduledTime": null
+#   }
 class ACTransitStopsObject(object):
   'This is the implementation of the object model used by ACTransit for their \
-  JSON object.'
+  Stops JSON object.'
 
   def __init__(self, json_obj):
     '''Takes the JSON obj (decoded to python dict) and extracts the stopId, \
@@ -69,16 +77,73 @@ class ACTransitStopsObject(object):
     'The scheduled time for the bus as per the AC Transit system.'
     return self.__scheduled_time
 
-  def __repr__(self):
-    'The repr() value of this object, can be used as python command directly.\
-    Of form (stopId, name, latitude, longitude, scheduled time)'
-    return '(%s, %s, %s, %s, %s)' % (self.__stopId, self.__name,\
-      self.__latitude, self.__longitude, self.__scheduled_time)
 
+# Predictions
+# JSON example -
+# {
+#     "StopId": 56707,
+#     "TripId": 5155418,
+#     "VehicleId": 5021,
+#     "RouteName": "19",
+#     "PredictedDelayInSeconds": 540,
+#     "PredictedDeparture": "2017-01-11T17:31:00",
+#     "PredictionDateTime": "2017-01-11T17:10:41"
+#   }
+class ACTransitPrediction(object):
+  'Wrapper class for the AC Transit\'s route prediction'
+
+  def __init__(self, json_obj):
+    'Takes the json_object representation(python dict) as the input and\
+    initializes the prediction object.'
+    self.__stopId = json_obj['StopId']
+    self.__tripId = json_obj['5155418']
+    self.__vehicleId = json_obj['VehicleId']
+    self.__route_name = json_obj['RouteName']
+    self.__predicted_delay_in_secs = json_obj['PredictedDelayInSeconds']
+    self.__predicted_departure = json_obj['PredictedDeparture']
+    self.__predicted_date_time = json_obj['PredictionDateTime']
+
+  @property
+  def stopId(self):
+    'The stopId as per AC Transit system.'
+    return self.__stopId
+
+  @property
+  def tripId(self):
+    'The tripId of the prediction as per the AC Transit system.'
+    return self.__tripId
+
+  @property
+  def vehicleId(self):
+    'The vehicleId of the prediction as per the AC Transit system.'
+    return self.__vehicleId
+
+  @property
+  def route_name(self):
+    'The route name of the prediction as per the AC Transit system.'
+    return self.__route_name
+
+  @property
+  def predicted_delay_in_seconds(self):
+    'The predicted delay in seconds as per the AC Transit system.'
+    return self.__predicted_delay_in_secs
+
+  @property
+  def predicted_departure(self):
+    'The predicted departure as per the AC Transit system.'
+    return self.__predicted_departure
+
+  @property
+  def predicted_date_time(self):
+    'The predicted data and time as per AC Transit system.'
+    return self.__predicted_date_time
 
 
 
 # API wrappers for AC Transit
+
+# GET stops
+# Retrieve all of AC Transit's currently active stops.
 def get_stops():
   'Gets all currently activ stops of the ACTransit system.'
   url = 'https://api.actransit.org/transit/stops/?token=%s' % __PERSONAL_API_KEY__
@@ -92,12 +157,33 @@ def get_stops():
     ac_transit_stops.append(ACTransitStopsObject(obj))
   return ac_transit_stops
 
+# GET stops/{latitude}/{longitude}/{distance}/{routeName}
+# Retrieve all active stops within a certain radius (in feet) 
+# of the given point. The default search radius is 500 feet.
+def get_active_stops(latitude, longitude, search_radius = 500):
+  'Retrieve all active stops within a certain radius (in feet) \
+  of the given point. The default search radius is 500 feet.'
+  pass
+
+# GET stops/{latitude}/{longitude}?distance={distance}&routeName={routeName}
+# Retrieve all active stops within a certain radius (in feet) 
+# of the given point. The default search radius is 500 feet.
+def get_active_stops_type2(latitude, longitude, search_radius = 500):
+  'Retrieve all active stops within a certain radius (in feet) \
+  of the given point. The default search radius is 500 feet.'
+  pass
+
+# GET stops/{stopId}/predictions  
+# Retrieve vehicle predictions for a particular stop.
+def get_predictions(stopId):
+  'Retrieve vehicle predictions for a particular stop.'
+  pass
 
 
 
 def main():
   'main function'
-  get_stops()
+  pass
 
 
 if __name__ == '__main__':
