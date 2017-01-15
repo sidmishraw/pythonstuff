f357c893220e8af0d2212cce1ef627aea66f96f2
Sidharth Mishra <sidmishraw@gmail.com>
2017-01-14 16:15:39 -0800

    removed the wrong nested project.

--- actransit_app/ac_transit_api_consumer/ac_transit_api_consumer/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = """Sidharth Mishra"""
-__email__ = 'sidharth.mishra@sjsu.edu'
-__version__ = '0.0.1'
--- actransit_app/ac_transit_api_consumer/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use AC Transit API Consumer in a project::
-
-    import ac_transit_api_consumer
