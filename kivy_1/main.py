3b5a5b757c1ac454465717ef7ed1d4a8bb047197
Sidharth Mishra <sidmishraw@gmail.com>
2017-04-09 14:30:55 -0700

    added eclipse settings

--- kivy_1/main.py
+++ kivy_1/main.py
@@ -1,4 +1,4 @@
-# The main.py as intended by Kivy. This is used as the main entry point it seems
+# main.py
 
 __author__ = 'sidmishraw'
 
@@ -6,13 +6,24 @@ import kivy
 kivy.require('1.9.1')
 
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.label import Label
+from kivy.graphics.texture import Texture
+from kivy.core import camera
+
+
+class Tex(Widget):
+
+  def __init__(self):
+    self.texture = Texture.create(size=(640, 480))
+
+
 
 
 class MyApp(App):
   'the application'
   def build(self):
-    return Label(text='App works xD!')
+    return Tex()
 
 
 
